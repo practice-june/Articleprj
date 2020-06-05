4b573c92ad429dc22abb147966e6c15b64b0759e
Sunhwa Cheon <Sunhwa@CheonSunhwaui-MacBook-Air.local>
2020-06-05 10:37:42 -0400

    0605

---
+++ test.js
@@ -0,0 +1,22 @@
+// var http = require('http');
+// var fs = require('fs');
+// var url = require('url');
+// const PORT = process.env.PORT || 5000
+
+// http.createServer(function(req, res){
+//     var q = url.parse(req.url, true);
+//     var filename = "." + q.pathname;
+//     if (filename == './'){filename = './index'}
+
+//     filename = filename + '.html';
+//     fs.readFile(filename, function(err, data){
+//         if(err){
+//             res.writeHead(404, {'content-Type' : 'text/html'});
+//             return res.end("404 Not found");
+//         }
+//         res.writeHead(200, {'content-Type' : 'text/html'});
+//         res.write(data);
+//         res.end();
+//     })
+    
+// }).listen(PORT);
