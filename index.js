7765794f2e729a8c4372b185db7f6f9b8ab8973b
Sunhwa Cheon <Sunhwa@CheonSunhwaui-MacBook-Air.local>
2020-06-06 12:10:07 -0400

    -2

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 const exphbs  = require('express-handlebars');
+// const request = require('request');
+
+
+
+// API key : pk_b21f9b78040b44bc9655b33b19e0595e 
+// function callApi(){
+//     request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_b21f9b78040b44bc9655b33b19e0595e', {json = true}, (err, res, body) =>{
+//     if(err){return console.log(err);}
+//     if(res.statusCode == 200){
+//     }
+// })
+// }
+
 
 // Set handlebar
 app.engine('handlebars', exphbs());
@@ -13,6 +26,11 @@ app.get('/', function (req, res) {
     res.render('home', {
         stuff: 'This is stuff'
     });
+});
+
+//creat about page route
+app.get('/about.html', function (req, res) {
+    res.render('about');
 });
 
 
